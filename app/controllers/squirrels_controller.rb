a7dee39e807ef06fc7dc43654f1053393ab2d7c3
Sam Blackman <sblackman@gmail.com>
2016-02-22 16:10:17 -0500

    change names to short_bio

--- app/controllers/squirrels_controller.rb
+++ app/controllers/squirrels_controller.rb
@@ -17,7 +17,7 @@ post '/squirrels' do
   @squirrel = Squirrel.new(build_squirrel_params(params))
   if request.xhr?
     if @squirrel.save
-      erb :'squirrels/_short', locals: {squirrel: @squirrel}, layout: false
+      erb :'squirrels/_short_bio', locals: {squirrel: @squirrel}, layout: false
     else
       status 409
       return @squirrel.errors.full_messages.to_json
